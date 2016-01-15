785cfacfd4e6a8e7d43c689808ccef2dd984f721
Anna Lawley <alawley1@gmail.com>
2016-01-15 13:49:37 -0800

    fix broken age evaluation js

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,11 @@ $(document).ready(function() {
   var vegas = 0
   var fiji = 0
   var mars = 0
-  var age = parseInt($("input#age").val());
 
   $(".qpage form").submit(function(event) {
     // Begin question evaluation code
+
     // Q1
-    alert("start pct = " + pct);
-    alert("start florence = " + florence);
-    alert("start vegas = " + vegas);
-    alert("start fiji = " + fiji);
-    alert("start mars = " + mars);
     var goals = $("select#goals").val();
     if (goals === "goals1") {
       pct +=2
@@ -22,6 +17,8 @@ $(document).ready(function() {
       fiji +=2
     } else if (goals === "goals3") {
       vegas +=2
+      pct -=1
+      mars -=1
     } else if (goals === "goals4") {
       pct +=1
       mars +=1
@@ -31,12 +28,6 @@ $(document).ready(function() {
       florence +=2
     } else { alert("We're sorry, something went wrong. Please answer the first question.");
     }
-    alert("q1 answer = " + goals);
-    alert("q1 pct = " + pct);
-    alert("q1 florence = " + florence);
-    alert("q1 vegas = " + vegas);
-    alert("q1 fiji = " + fiji);
-    alert("q1 mars = " + mars);
     // Q2
     var spend = $("select#spend").val();
     if (spend === "spend1") {
@@ -54,12 +45,6 @@ $(document).ready(function() {
       vegas +=1
     } else { alert("We're sorry, something went wrong. Please answer the second question.");
     }
-    alert("q2 answer = " + spend);
-    alert("q2 pct = " + pct);
-    alert("q2 florence = " + florence);
-    alert("q2 vegas = " + vegas);
-    alert("q2 fiji = " + fiji);
-    alert("q2 mars = " + mars);
     // Q3
     var drink = $("select#drink").val();
     if (drink === "drink1") {
@@ -74,12 +59,6 @@ $(document).ready(function() {
       vegas +=2
     } else { alert("We're sorry, something went wrong. Please refresh and answer the third question.");
     }
-    alert("q3 answer = " + drink);
-    alert("q3 pct = " + pct);
-    alert("q3 florence = " + florence);
-    alert("q3 vegas = " + vegas);
-    alert("q3 fiji = " + fiji);
-    alert("q3 mars = " + mars);
     // Q4
     var unique = $("select#unique").val();
     if (unique === "unique1") {
@@ -93,34 +72,22 @@ $(document).ready(function() {
       mars -=2
     } else { alert("We're sorry, something went wrong. Please refresh and answer the fourth question.");
     }
-    alert("q4 answer = " + unique);
-    alert("q4 pct = " + pct);
-    alert("q4 florence = " + florence);
-    alert("q4 vegas = " + vegas);
-    alert("q4 fiji = " + fiji);
-    alert("q4 mars = " + mars);
     //Q5
-    // if (age < 18) {
-    //   $(".minor").show();
-    // } else if {
-    //   (age <= 35) {
-    //     pct +=1
-    //     vegas +=1
-    //     mars +=1
-    //   }
-    // } else {
-    //   florence +=1
-    //   fiji +=1
-    // }
-    // if (age <21 && age >=18) {
-    //   vegas -=4
-    // } else {}
+    var age = parseInt($("input#age").val());
+    if (age <= 35) {
+        pct +=1
+        vegas +=1
+        mars +=1
+    } else {
+      florence +=1
+      fiji +=1
+    }
+    if (age <21 && age >=18) {
+      vegas -=4
+    } else {
+      vegas +=1
+    }
     // end question evaluation code
-    alert("pct = " + pct);
-    alert("florence = " + florence);
-    alert("vegas = " + vegas);
-    alert("fiji = " + fiji);
-    alert("mars = " + mars);
 
     // begin show results code
     if (florence >= fiji && florence >= vegas && florence >= pct && florence >= mars) {
@@ -139,10 +106,25 @@ $(document).ready(function() {
       $(".qpage").hide();
       $(".results").show();
       $(".pct").show();
-    } else {
+    } else if (mars > florence && mars > fiji && mars > vegas && mars > pct){
       $(".qpage").hide();
       $(".results").show();
       $(".mars").show();
+    } else {
+      $(".qpage").hide();
+      $(".results").show();
+      $(".minor").show();
+    }
+    // makes sure minors won't get other results
+    if (age < 18) {
+      $(".qpage").hide();
+      $(".results").show();
+      $(".florence").hide();
+      $(".fiji").hide();
+      $(".vegas").hide();
+      $(".pct").hide();
+      $(".mars").hide();
+      $(".minor").show();
     }
     // end show results code
     event.preventDefault();
